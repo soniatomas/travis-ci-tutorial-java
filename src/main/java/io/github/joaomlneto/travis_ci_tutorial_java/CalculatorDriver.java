e31dea88fa6ba2fd07e69e2d8bf8555162142473
Sonia Tomas <Sonia@pcp135076pcs.wireless.calpoly.edu>
2016-09-28 12:19:58 -0700

    ..

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/CalculatorDriver.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/CalculatorDriver.java
@@ -5,6 +5,7 @@ public class CalculatorDriver {
 	public static void main(String[] args) {
 		SimpleCalculator calc = new SimpleCalculator();
 		System.out.println(calc.mul(calc.mul(calc.mul(calc.mul(10,3),7), 13), 37));
+	
 	}
 
 }
